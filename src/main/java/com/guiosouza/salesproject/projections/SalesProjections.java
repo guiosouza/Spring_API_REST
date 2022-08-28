075d459b55852f5562aa1ac6568320b428fa3638
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2022-08-28 19:35:54 -0300

    começando a implementar a query

---
+++ src/main/java/com/guiosouza/salesproject/projections/SalesProjections.java
@@ -0,0 +1,7 @@
+package com.guiosouza.salesproject.projections;
+
+import java.util.Date;
+
+public interface SalesProjections {
+	Date getSale_date();
+}
--- src/main/java/com/guiosouza/salesproject/repositories/SalesRepository.java
+++ src/main/java/com/guiosouza/salesproject/repositories/SalesRepository.java
@@ -1,11 +1,20 @@
 package com.guiosouza.salesproject.repositories;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.guiosouza.salesproject.entities.Sales;
+import com.guiosouza.salesproject.projections.SalesProjections;
 
 @Repository
 public interface SalesRepository extends JpaRepository<Sales, Long>{
+	// buscar vendas do seller para tirar média
+	@Query(nativeQuery = true, value = "SELECT * FROM SALES\r\n"
+			+ "WHERE SALE_DATE  BETWEEN (:initialDate) AND (:finalDate);")
+	List<SalesProjections> findNumbersOfSales(Date initialDate, Date finalDate);
 
 }
